d93ffea7103b8f52cef8b0226b0bc980a963f526
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-05-04 22:47:05 +0800

    add: login register logout

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,6 +7,30 @@ module Candyland
   # Web controller for Candyland API
   class App < Roda
     route('auth') do |routing|
+      routing.is 'register' do
+        # GET /auth/register
+        routing.get do
+          view :register
+        end
+
+        # POST /auth/register
+        routing.post do
+          account = RegisterAccount.new(App.config).call(
+            username: routing.params['username'],
+            email: routing.params['email'],
+            password: routing.params['password']
+          )
+
+          session[:current_account] = account
+          flash[:notice] = "Welcome! #{account['username']}!"
+          routing.redirect '/'
+        rescue StandardError
+          flash.now[:error] = 'Account information invalid'
+          response.status = 400
+          view :register
+        end
+      end
+
       @login_route = '/auth/login'
       routing.is 'login' do
         # GET /auth/login
