c4461707be0f6b9918ee4304ccaa737d53460c9d
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-05-04 23:20:50 +0800

    add: account nav

--- app/controllers/account.rb
+++ app/controllers/account.rb
@@ -4,11 +4,11 @@ require 'roda'
 require_relative './app'
 
 module Candyland
-  # Web controller for Credence API
+  # Web controller for Candyland API
   class App < Roda
     route('account') do |routing|
       routing.on do
-        # GET /account/login
+        # GET /account/:username
         routing.get String do |username|
           if @current_account && @current_account['username'] == username
             view :account, locals: { current_account: @current_account }
