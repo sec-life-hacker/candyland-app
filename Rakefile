3ebc7dd389bb91b1df4b4545d1038fe6991b639b
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-05-11 23:51:05 +0800

    add: secured session

--- Rakefile
+++ Rakefile
@@ -35,7 +35,7 @@ task :audit do
 end
 
 desc 'Checks for release'
-task :release => [:spec, :style, :audit] do
+task :release => %i[spec style audit] do
   puts "\nReady for release!"
 end
 
--- app/lib/secure_session.rb
+++ app/lib/secure_session.rb
@@ -1,46 +1,44 @@
 # frozen_string_literal: true
 
-require 'base64'
-require 'rbnacl'
+require 'redis'
+require_relative 'secure_message'
 
-# Encrypt and Decrypt from Database
-class SecureMessage
-  # Generate key for Rake tasks (typically not called at runtime)
-  def self.encoded_random_bytes(length)
-    bytes = RbNaCl::Random.random_bytes(length)
-    Base64.strict_encode64 bytes
+# Encrypt and Decrypt JSON encoded sessions
+class SecureSession
+  ## Any use of this library must setup configuration information
+  def self.setup(redis_url)
+    @redis_url = redis_url
   end
 
-  def self.generate_key
-    encoded_random_bytes(RbNaCl::SecretBox.key_bytes)
+  ## Class methods to create and retrieve cookie salt
+  SESSION_SECRET_BYTES = 64
+
+  # Generate secret for sessions
+  def self.generate_secret
+    SecureMessage.encoded_random_bytes(SESSION_SECRET_BYTES)
   end
 
-  # Call setup once to pass in MSG_KEY env var
-  def self.setup(msg_key)
-    @msg_key = msg_key
+  def self.wipe_redis_sessions
+    redis = Redis.new(url: @redis_url)
+    redis.each_key { |session_id| redis.del session_id }
   end
 
-  def self.key
-    @key ||= Base64.strict_decode64(@msg_key)
+  ## Instance methods to store and retrieve encrypted session data
+  def initialize(session)
+    @session = session
   end
 
-  # Encrypt or else return nil if data is nil
-  def self.encrypt(message)
-    return nil unless message
-
-    message_json = message.to_json
-    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
-    ciphertext = simple_box.encrypt(message_json)
-    Base64.urlsafe_encode64(ciphertext)
+  def set(key, value)
+    @session[key] = SecureMessage.encrypt(value)
   end
 
-  # Decrypt or else return nil if database value is nil already
-  def self.decrypt(ciphertext64)
-    return nil unless ciphertext64
+  def get(key)
+    return nil unless @session && @session[key]
 
-    ciphertext = Base64.urlsafe_decode64(ciphertext64)
-    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
-    message_json = simple_box.decrypt(ciphertext)
-    JSON.parse(message_json)
+    SecureMessage.decrypt(@session[key])
+  end
+
+  def delete(key)
+    @session.delete(key)
   end
 end
