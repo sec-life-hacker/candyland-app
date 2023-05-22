81bfc7deaa091c2f22cd4f8068b3246379e16b5d
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-05-22 17:00:58 +0800

    auth token

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -6,32 +6,7 @@ require_relative './app'
 module Candyland
   # Web controller for Candyland API
   class App < Roda
-    # rubocop:disable Metrics/BlockLength
     route('auth') do |routing|
-      routing.is 'register' do
-        # GET /auth/register
-        routing.get do
-          view :register
-        end
-
-        # POST /auth/register
-        routing.post do
-          account = RegisterAccount.new(App.config).call(
-            username: routing.params['username'],
-            email: routing.params['email'],
-            password: routing.params['password']
-          )
-
-          session[:current_account] = account
-          flash[:notice] = "Welcome! #{account['username']}!"
-          routing.redirect '/'
-        rescue StandardError
-          flash.now[:error] = 'Account information invalid'
-          response.status = 400
-          view :register
-        end
-      end
-
       @login_route = '/auth/login'
       routing.is 'login' do
         # GET /auth/login
@@ -41,28 +16,77 @@ module Candyland
 
         # POST /auth/login
         routing.post do
-          account = AuthenticateAccount.new(App.config).call(
+          account_info = AuthenticateAccount.new(App.config).call(
             username: routing.params['username'],
             password: routing.params['password']
           )
 
-          session[:current_account] = account
-          flash[:notice] = "Welcome back #{account['username']}!"
-          routing.redirect '/'
-        rescue StandardError
-          flash.now[:error] = 'Username and password did not match our records'
-          response.status = 400
-          view :login
-        end
-      end
+          current_account = Account.new(
+            account_info[:account],
+            account_info[:auth_token]
+          )
 
-      routing.on 'logout' do
-        routing.get do
-          session[:current_account] = nil
+          CurrentSession.new(session).current_account = current_account
+
+          flash[:notice] = "Welcome back #{current_account.username}!"
+          routing.redirect '/'
+        rescue AuthenticateAccount::UnauthorizedError
+          flash.now[:error] = 'Username and password did not match our records'
+          response.status = 401
+          view :login
+        rescue AuthenticateAccount::ApiServerError => e
+          App.logger.warn "API server error: #{e.inspect}\n#{e.backtrace}"
+          flash[:error] = 'Our servers are not responding -- please try later'
+          response.status = 500
           routing.redirect @login_route
         end
       end
+
+      @logout_route = '/auth/logout'
+      routing.is 'logout' do
+        # GET /auth/logout
+        routing.get do
+          CurrentSession.new(session).delete
+          flash[:notice] = "You've been logged out"
+          routing.redirect @login_route
+        end
+      end
+
+      @register_route = '/auth/register'
+      routing.on 'register' do
+        routing.is do
+          # GET /auth/register
+          routing.get do
+            view :register
+          end
+
+          # POST /auth/register
+          routing.post do
+            account_data = routing.params.transform_keys(&:to_sym)
+            VerifyRegistration.new(App.config).call(account_data)
+
+            flash[:notice] = 'Please check your email for a verification link'
+            routing.redirect '/'
+          rescue VerifyRegistration::ApiServerError => e
+            App.logger.warn "API server error: #{e.inspect}\n#{e.backtrace}"
+            flash[:error] = 'Our servers are not responding -- please try later'
+            routing.redirect @register_route
+          rescue StandardError => e
+            App.logger.error "Could not process registration: #{e.inspect}"
+            flash[:error] = 'Registration process failed -- please try later'
+            routing.redirect @register_route
+          end
+        end
+
+        # GET /auth/register/<token>
+        routing.get(String) do |registration_token|
+          flash.now[:notice] = 'Email Verified! Please choose a new password'
+          new_account = SecureMessage.decrypt(registration_token)
+          view :register_confirm,
+               locals: { new_account:,
+                         registration_token: }
+        end
+      end
     end
-    # rubocop:enable Metrics/BlockLength
   end
 end
